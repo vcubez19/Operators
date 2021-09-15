ef7e99867c414fd21c00203ba9b034d21d73d120
Vincent Cubit <vinniecubit@yahoo.com>
2021-09-15 02:12:29 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val a = 4
+    val b = 7
+
+    println(a + b != 44)
+    println(a == b)
+    println(a + b - 4 == b)
+    println(a > b || b * 2 > 10)
+    println(a % b + a - 1 == b)
+    println((a + 10) / b == 2)
+
+}
